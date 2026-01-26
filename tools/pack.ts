00eb181e9aebddd461dd75ccaa9b3d69a776ae4e
Sylvain Cau <10268458+sylc@users.noreply.github.com>
2026-01-26 09:01:57 +0800

    chore: fix ci build (#27)
    * x
    
    * x
    
    * trim version

--- tools/pack.ts
+++ tools/pack.ts
@@ -17,6 +17,6 @@ copySync("./tak.exe", "./dist/msix/tak.exe", { overwrite: true });
 let appxmanifest = Deno.readTextFileSync("./dist/msix/appxmanifest.xml");
 appxmanifest = appxmanifest.replace(
   'Version="0.0.0.0"',
-  `Version="${version.replace("v", "").replace(" ", "")}.0"`,
+  `Version="${version.replace("v", "").trim()}.0"`,
 );
 Deno.writeTextFileSync("./dist/msix/appxmanifest.xml", appxmanifest);
